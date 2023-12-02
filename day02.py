b71a8ea807c59036eacda533f5281f4084125f44
Juan Lopes <juan.lopes@zwift.com>
2023-12-02 11:12:08 -0300

    Fixes

--- day02.py
+++ day02.py
@@ -1,16 +1,13 @@
-import sys
-def colors(line):
-    D = {}
-    for draw in line.split(';'):
-        for ball in draw.split(','):
-            number, color = ball.split()
-            D[color] = max(D.get(color, 0), int(number))
-    return [D[x] for x in ('red', 'green', 'blue')]
-    
+import sys, math
+
 total1, total2 = 0, 0
 for line in sys.stdin.read().splitlines():
     game, line = line.split(':')
-    c = colors(line)
-    total1 += int(game.split()[1]) if c[0] <= 12 and c[1] <= 13 and c[2] <= 14 else 0
-    total2 += c[0] * c[1] * c[2]
+    C = {}
+    for draw in line.split(';'):
+        for ball in draw.split(','):
+            number, color = ball.split()
+            C[color] = max(C.get(color, 0), int(number))
+    total1 += int(game.split()[1]) if C['red'] <= 12 and C['green'] <= 13 and C['blue'] <= 14 else 0
+    total2 += math.prod(C.values())
 print(total1, total2)
