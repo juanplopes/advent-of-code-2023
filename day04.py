08f43435b2f99e03fe33144c9ac47f6cf46d71d0
Juan Lopes <juan.lopes@zwift.com>
2023-12-04 12:49:58 -0300

    Day 04

--- day04.py
+++ day04.py
@@ -3,8 +3,8 @@ total1, total2, C = 0, 0, {}
 for i, line in enumerate(sys.stdin.read().splitlines()):
     winning, mine = (x.split() for x in line.split(':')[1].split('|'))
     common = len(set(mine) & set(winning))
-    total2 += C.get(i, 1)
-    total1 += 2**(common-1) if common else 0
     for j in range(i+1, i+common+1):
         C[j] = C.get(j, 1) + C.get(i, 1)
+    total1 += 2**(common-1) if common else 0
+    total2 += C.get(i, 1)
 print(total1, total2)
