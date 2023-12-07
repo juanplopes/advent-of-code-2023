4941acc4a2cbb59feb90dfc353a73653698daeaf
Juan Lopes <juan.lopes@zwift.com>
2023-12-06 21:37:00 -0300

    Optimize Day 06

--- day06.py
+++ day06.py
@@ -1,7 +1,9 @@
 import sys, math
 lines = sys.stdin.read().splitlines()
 def solve(time, dist):
-    return sum((time-j)*j>dist for j in range(1, time))
+    a = math.floor((time-(time**2-4*dist)**0.5)/2+1)
+    b = math.ceil((time+(time**2-4*dist)**0.5)/2-1)
+    return b-a+1
 T, D = (list(map(int, x.split(':')[1].split())) for x in lines)
 answer1 = math.prod(solve(T[i], D[i]) for i in range(len(T)))
 answer2 = solve(*(int(x.split(':')[1].replace(' ', '')) for x in lines))
