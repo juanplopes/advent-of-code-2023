309c6dc8d7695f53a87bcba785b46ed2f14526e1
Juan Lopes <juan.lopes@zwift.com>
2023-12-08 19:34:50 -0300

    Day 08

--- day08.py
+++ day08.py
@@ -5,6 +5,6 @@ G = {x[0]: [x[1], x[2]] for x in (re.split('[ =(),]+', line) for line in T)}
 def distance(start):
     for i in range(10000000):
         if start.endswith('Z'): return i
-        start = G[start][instr[(i)%len(instr)] == 'R']
+        start = G[start][instr[i%len(instr)] == 'R']
 
 print(distance('AAA'), math.lcm(*(distance(x) for x in G if x[-1] == 'A')))
