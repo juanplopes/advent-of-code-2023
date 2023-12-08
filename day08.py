816fd558c649f2f397e22d5ba52c6838f8ec5434
Juan Lopes <juan.lopes@zwift.com>
2023-12-08 19:28:44 -0300

    Day 08

--- day08.py
+++ day08.py
@@ -1,13 +1,10 @@
 import sys, re, math
-T = sys.stdin.read().splitlines()
-instr = T[0]
-G = {x[0]: [x[1], x[2]] for x in (
-    re.split('[ =(),]+', line) for line in T[2:])}
+instr, _, *T = sys.stdin.read().splitlines()
+G = {x[0]: [x[1], x[2]] for x in (re.split('[ =(),]+', line) for line in T)}
 
-def distance(start, end):
+def distance(start):
     for i in range(10000000):
-        if start in end: return i
+        if start.endswith('Z'): return i
         start = G[start][instr[(i)%len(instr)] == 'R']
 
-print(distance('AAA', ['ZZZ']), math.lcm(*(
-    distance(x, {x for x in G if x[-1] == 'Z'}) for x in G if x[-1] == 'A')))
+print(distance('AAA'), math.lcm(*(distance(x) for x in G if x[-1] == 'A')))
