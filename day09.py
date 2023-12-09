b807377cb0e57ba6b4199b66f9b27d1fd3f1cd65
Juan Lopes <juan.lopes@zwift.com>
2023-12-09 11:16:42 -0300

    Day 09

--- day09.py
+++ day09.py
@@ -1,8 +1,8 @@
 import sys
 T = [list(map(int, x.split())) for x in sys.stdin.read().splitlines()]
 
-def solve(line, idx, m):
+def solve(line):
     if set(line) == {0}: return 0
-    return line[idx] + m * solve([b-a for a, b in zip(line, line[1:])], idx, m)
+    return line[-1] + solve([b-a for a, b in zip(line, line[1:])])
 
-print(sum(solve(x, -1, 1) for x in T), sum(solve(x, 0, -1) for x in T))
+print(sum(solve(x) for x in T), sum(solve(x[::-1]) for x in T))
